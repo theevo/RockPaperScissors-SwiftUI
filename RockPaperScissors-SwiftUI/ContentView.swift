0e7e44584071d9705bd3b8b6a7ee3a426e1a2d15
Theo Vora <theo.vora@gmail.com>
2021-07-01 20:57:42 -0600

    Display layout

--- RockPaperScissors-SwiftUI/ContentView.swift
+++ RockPaperScissors-SwiftUI/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let moves = ["Rock", "Paper", "Scissors"]
+    let finalRound = 10
+    
+    @State var shouldWin = false
+    @State var round = 1
+    @State var points = 0
+    @State var playerMove = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("Score: \(points)")
+            }
+            Spacer()
+            VStack {
+                Text("Choose a move where you")
+                    .fontWeight(.light)
+                HStack(alignment: .bottom) {
+                    Text(shouldWin ? "Win" : "Lose")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .italic()
+                    Text("to")
+                    Text("\(moves[0])")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .italic()
+                }
+            }
+            Spacer()
+            VStack {
+                Picker("Your move", selection: $playerMove) {
+                    ForEach(0 ..< moves.count) { i in
+                        Text("\(moves[i])")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                Button("Play") {
+                    print("Player chose \(moves[playerMove])")
+                }
+            }
+            Spacer()
+        }
     }
 }
 
