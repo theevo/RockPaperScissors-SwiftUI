a76ce94d40df9ba1fa209fa17fce36d0e490f74e
Theo Vora <theo.vora@gmail.com>
2021-07-01 20:17:48 -0600

    Initial Commit

---
+++ RockPaperScissors-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors-SwiftUI
+//
+//  Created by Theo Vora on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors-SwiftUI/RockPaperScissors_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors_SwiftUIApp.swift
+//  RockPaperScissors-SwiftUI
+//
+//  Created by Theo Vora on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
